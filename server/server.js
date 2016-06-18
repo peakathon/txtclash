16ec7ad9b0541024e1b425a9cafcc7accd090d68
Malek Ascha <malekascha@gmail.com>
2016-06-17 18:13:57 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(__dirname + '/../client'))
+
+app.listen(port);
